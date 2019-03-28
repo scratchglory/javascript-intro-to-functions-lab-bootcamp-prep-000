a37d54a6cbd87b4d9af1b14bcc15f98b8f237041
Adella <adellakenn@gmail.com>
2019-03-28 00:51:55 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string) {
+ return string.toUpperCase() 
+}
+
+'Hello!'.toUpperCase() //'Hello!'
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+'HELLO!'.toLowerCase()
+
+function logShout(string) {
+  console.log(string)
+}
+'hello'.toUpperCase
