d6c8b4bfb2c82a2f5099ede2e25930547c25f1ed
Adella <adellakenn@gmail.com>
2019-03-28 01:00:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
 
 function sayHiToGrandma(string) {
   string===(string.toLowerCase())
   return "I can't hear you!"
 }
+
 
