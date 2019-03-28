f88f1f6071fdd8125eed6f0dc903831c1148d301
Adella <adellakenn@gmail.com>
2019-03-28 01:21:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ function shout(string) {
  return string.toUpperCase() 
 }
 
-'Hello!'.toUpperCase() //'Hello!'
-
-
 function whisper(string) {
   return string.toLowerCase()
 }
-'HELLO!'.toLowerCase()
 
 function logShout(string) {
   console.log(string.toUpperCase())
